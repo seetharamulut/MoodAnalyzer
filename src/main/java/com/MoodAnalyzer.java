69ed6798f03c5c8a20ecd4de3d53cd68294d00fe
Seetharamulu T <thumurugoti.seetharamulu@gmail.com>
2020-06-20 18:50:11 +0530

    Added: exception handling for null input

--- src/main/java/com/MoodAnalyzer.java
+++ src/main/java/com/MoodAnalyzer.java
@@ -2,16 +2,21 @@ package com;
 
 public class MoodAnalyzer {
     String message;
-    public MoodAnalyzer(String message){
+
+    public MoodAnalyzer(String message) {
         this.message = message;
     }
+
     public String analyzeMood() {
+        try {
             if (message.contains("Im in sad mood")) {
                 return "Sad";
             } else {
                 return "Happy";
             }
-
+        } catch (NullPointerException e) {
+            return "Happy";
         }
     }
+}
 
---
+++ src/main/java/com/MoodAnalyzerException.java
@@ -0,0 +1,7 @@
+package com;
+
+public class MoodAnalyzerException extends RuntimeException{
+    public MoodAnalyzerException(String message){
+        super(message);
+    }
+}
